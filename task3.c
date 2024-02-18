f73951ebca3435718897315777b579dfab7b9723
Uneld <aleksey.minchenya@gmail.com>
2024-02-18 22:44:07 +0300

    update task3

--- task3.c
+++ task3.c
@@ -5,7 +5,7 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     
-    if (a < b && b < c) {
+    if (a <= b && b <= c) {
         printf("YES");
     } else {
         printf("NO");
